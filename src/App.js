6485d277eac840357d27bd488d74f12b551fef93
Jailson Prado <jailsondpd@gmail.com>
2022-02-06 17:58:14 -0300

    Organizacao de estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-     <h1>Blog react js</h1>
+      <h1>Blog react js</h1>
     </div>
   );
 }
 
-export default App;
+
