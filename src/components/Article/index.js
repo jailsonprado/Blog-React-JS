00246880daef8764b8f11c22eb3ede6ac7f86d4c
Jailson Prado <jailsondpd@gmail.com>
2022-02-06 23:27:59 -0300

    create API using Json-server

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -6,23 +6,8 @@ const Article = () => {
            <h3>Articles</h3>
            <div className="container-posts mt-5">
             <Post subtitle="tecnologia" title="O guia definitivo do Blog">
-                Sint cillum adipisicing excepteur duis velit amet occaecat qui ullamco fugiat.
-                Consectetur do mollit Lorem enim aliqua sunt voluptate laborum.
-                Ea pariatur non excepteur sint pariatur consectetur exercitation non fugiat.
-                Commodo quis adipisicing ad aliqua consectetur sint irure est sint.
+                
             </Post> 
-            <Post subtitle="fotografia" title="Quais as melhores cameras em 2021?">
-                Sint cillum adipisicing excepteur duis velit amet occaecat qui ullamco fugiat.
-                Consectetur do mollit Lorem enim aliqua sunt voluptate laborum.
-                Ea pariatur non excepteur sint pariatur consectetur exercitation non fugiat.
-                Commodo quis adipisicing ad aliqua consectetur sint irure est sint.
-            </Post>
-            <Post subtitle="cinema" title="Os 10 filmes com maior bilheteria">
-                Sint cillum adipisicing excepteur duis velit amet occaecat qui ullamco fugiat.
-                Consectetur do mollit Lorem enim aliqua sunt voluptate laborum.
-                Ea pariatur non excepteur sint pariatur consectetur exercitation non fugiat.
-                Commodo quis adipisicing ad aliqua consectetur sint irure est sint.
-            </Post>
            </div>
           
        </section>
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,24 +1,44 @@
-const Post = (props) => {
-    return (
-        <div className="post mx-2">
-            <div className="img-post">
+import {useEffect, useState} from 'react'
 
-            </div>
-            <h5>{props.subtitle}</h5>
-            <h3>{props.title}</h3>
-            <p className="mt-1">
-                {props.children}
-            </p>
-            <div className="flex pt-2">
-                <div className="img-profile"></div>
-                <div className="desc-profile ml-2">
-                    <h6 className="color-blue">Fulano de tal</h6>
-                    <p>
-                        Aug 2, 2020 - 8 min read
-                    </p>
+
+const Post = (props) => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:4000/posts')
+        .then((response) => response.json())
+        .then((response) => {
+            setPosts(response);
+        })
+    })
+
+
+    return (
+        <>
+        {posts.map((item) => {
+            return (
+                <div className="post mx-2" key={item.id}>
+                <div className="img-post">
+    
+                </div>
+                <h5>{item.category}</h5>
+                <h3>{props.title}</h3>
+                <p className="mt-1">
+                    {item.description}
+                </p>
+                <div className="flex pt-2">
+                    <div className="img-profile"></div>
+                    <div className="desc-profile ml-2">
+                        <h6 className="color-blue">{item.author}</h6>
+                        <p>
+                            {item.date}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+            )
+        })}
+       </>
     )
 }
 
