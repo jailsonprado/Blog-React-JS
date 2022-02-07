86ce595fac37267655d315e3fab206371abfe8ab
Jailson Prado <jailsondpd@gmail.com>
2022-02-06 21:32:06 -0300

    create method for posts, styles and props only of components

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,11 +2,30 @@ import Post from '../Post';
 
 const Article = () => {
     return (
-        <>
-            
-            <h1>Article</h1>
-            <Post/>
-        </>
+       <section className="container">
+           <h3>Articles</h3>
+           <div className="container-posts mt-5">
+            <Post subtitle="tecnologia" title="O guia definitivo do Blog">
+                Sint cillum adipisicing excepteur duis velit amet occaecat qui ullamco fugiat.
+                Consectetur do mollit Lorem enim aliqua sunt voluptate laborum.
+                Ea pariatur non excepteur sint pariatur consectetur exercitation non fugiat.
+                Commodo quis adipisicing ad aliqua consectetur sint irure est sint.
+            </Post> 
+            <Post subtitle="fotografia" title="Quais as melhores cameras em 2021?">
+                Sint cillum adipisicing excepteur duis velit amet occaecat qui ullamco fugiat.
+                Consectetur do mollit Lorem enim aliqua sunt voluptate laborum.
+                Ea pariatur non excepteur sint pariatur consectetur exercitation non fugiat.
+                Commodo quis adipisicing ad aliqua consectetur sint irure est sint.
+            </Post>
+            <Post subtitle="cinema" title="Os 10 filmes com maior bilheteria">
+                Sint cillum adipisicing excepteur duis velit amet occaecat qui ullamco fugiat.
+                Consectetur do mollit Lorem enim aliqua sunt voluptate laborum.
+                Ea pariatur non excepteur sint pariatur consectetur exercitation non fugiat.
+                Commodo quis adipisicing ad aliqua consectetur sint irure est sint.
+            </Post>
+           </div>
+          
+       </section>
     )
 }
 
